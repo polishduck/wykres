c0b1a081b5c30784053cc70f6d3df4e44dfc0aae
Patryk Kaczmarek <jrduck@kaczor-pc.(none)>
2013-06-03 02:12:38 +0200

    some adds

--- src/app/view/mainView.java
+++ src/app/view/mainView.java
@@ -2,6 +2,7 @@ package app.view;
 
 import info.monitorenter.gui.chart.Chart2D;
 import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.traces.Trace2DLtd;
 import info.monitorenter.gui.chart.traces.Trace2DSimple;
 
 import java.awt.Color;
@@ -18,6 +19,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -32,24 +35,27 @@ import app.model.*;
 
 public class mainView extends JFrame{
 	
+	public Timer timer;
+    public TimerTask task;
+    public int jj=0;
+    
 	public mainView(mainController m){
 		setTitle("Wyswietlanie wykresow");
 		setBackground(Color.white);
 		setSize(400,300);
-		final float[] data = new float[10];
-		data[0] = 1;
-		data[1] = 1;
-		data[2] = 1;
-		data[3] = 1;
-		data[4] = 1;
-		data[5] = 1;
-		data[6] = 1;
-		data[7] = 1;
-		data[8] = 1;
-		data[9] = 1;
+		final float[] data = new float[1000];
+		for (int k=1; k<1000; k++) {
+			data[k] = 0;				
+		}
 		
-		final Chart2D chart = new Chart2D();
-		final ITrace2D trace = new Trace2DSimple();
+
+		Chart2D chart = new Chart2D();
+		final ITrace2D trace = new Trace2DLtd(200);
+		trace.setColor(Color.BLUE);
+
+		chart.addTrace(trace); 
+//		
+
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -76,18 +82,12 @@ public class mainView extends JFrame{
 						  BufferedReader br = new BufferedReader(new InputStreamReader(in));
 						  String strLine;
 						  while ((strLine = br.readLine()) != null)   {
-							  System.out.println("w while");
 							  // Print the content on the console
-							  for(int ii=10;ii>0;ii--){
-								  System.out.println("w forze");
-									try {
-										trace.addPoint(ii,3*10.0+ii);
-									} catch
-					//			  	data[ii] = Float.parseFloat(strLine);
-									System.out.println ("data od i:" + data[ii]); 
+							  for(int ii=0;ii<1000;ii++){
+							//		trace.addPoint(ii,3*10.0+ii);
+								  	data[ii] = Float.parseFloat(strLine);
 							  }
 
-							  System.out.println (strLine);
 						  }
 					} catch (FileNotFoundException e1) {
 						// TODO Auto-generated catch block
@@ -112,18 +112,32 @@ public class mainView extends JFrame{
 		
 		setVisible(true);
 		
-
-		chart.addTrace(trace); 
 		getContentPane().add(chart);
-		
 		int size = data.length;
-		System.out.println("dlugosc:" + size + "\n");
-/*
- * 		for(int i=size;i>=0;i--){
-	//		trace.addPoint(i,data[i]);
-		}
- */
+	  
+		timer = new Timer(true);
+	    task = new TimerTask() {
 
-		
+	      private double m_y = 0;
+	      private long m_starttime = System.currentTimeMillis();
+	      /**
+	       * @see java.util.TimerTask#run()
+	       */
+
+	      @Override
+	      public void run() {
+	        double rand = Math.random();
+	        boolean add = (rand >= 0.5) ? true : false;
+	        
+	        this.m_y = (add) ? this.m_y + Math.random() : this.m_y - Math.random();
+	        trace.addPoint(jj, data[jj]);
+	        jj=jj+1;
+	        if (jj == 1000){
+	        	System.out.println("wychodzi");
+	        	cancel();
+	        }
+	       }
+	    };
+		timer.schedule(task, 10, 20);
 	}
 }
